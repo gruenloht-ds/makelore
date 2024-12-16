8334dd83578329c13aa203b77890c85122dd29bb
gruenloht-ds <tim@gruenloh.org>
2024-12-16 00:04:09 -0600

    cleaned up scripts

--- Data_Processing/scrape_tes_names.R
+++ Data_Processing/scrape_tes_names.R
@@ -4,68 +4,8 @@ library(rvest)
 # Web scrape character names from the elder scrolls (TES) games
 # https://en.uesp.net/wiki/Main_Page
 
-#### SKYRIM ####
-url <- "https://en.uesp.net/wiki/Skyrim:People"
-html_page <- read_html(url)
-
-# Note dragons and other creatures are not scraped in this search
-
-# This match will miss people from random encounters as these people are formatted in a list
-table_cells <- html_page %>%
-  html_nodes('td > b a') %>%
-  html_text()
-
-# Check for people scraped
-table_cells[str_detect(tolower(table_cells), 'serana')]
-table_cells[str_detect(tolower(table_cells), 'harkon')]
-table_cells[str_detect(tolower(table_cells), 'ebony')]
-table_cells[str_detect(tolower(table_cells), "j'zargo")]
-table_cells[str_detect(tolower(table_cells), "'")]
-
-# Match for people who occur during random encounters
-random_encounter <- html_page %>% 
-  html_nodes('li b a') %>% 
-  html_text()
-
-skyrim_people <- tibble(name = c(table_cells, random_encounter))
-
-# Remove duplicates (people might have lived in multiple different places resulting in multiple mentions)
-skyrim_people <- skyrim_people %>% 
-  group_by(name) %>% 
-  filter(row_number() == 1) %>% 
-  ungroup
-
-print(paste0('There are a total of ', nrow(skyrim_people), ' Skyrim characters'))
-# "There are a total of 1031 Skyrim characters"
-
-
-#### OBLIVION ####
-url <- "https://en.uesp.net/wiki/Oblivion:People"
-html_page <- read_html(url)
-
-# This match will miss people who do not physically show up in the game
-# This will be fine as some of those names are joke names
-# Only miss out on an additional 40 potential names
-table_cells <- html_page %>%
-  html_nodes(".wikitable dt a , b a") %>%
-  html_text()
-
-oblivion_people <- tibble(name = c(table_cells))
-
-# Remove duplicates (people might have lived in multiple different places resulting in multiple mentions)
-oblivion_people <- oblivion_people %>% 
-  group_by(name) %>% 
-  filter(row_number() == 1) %>% 
-  ungroup
-
-print(paste0('There are a total of ', nrow(oblivion_people), ' Oblivion characters'))
-# "There are a total of 921 Oblivion characters"
-
-#### MORROWIND ####
-
-# The morrowind layout is different. There are subpages the contain lists of npc names
-# Create a function to cycle through these pages since the layout in each subpage is the same
-pull_names <- function(url, extract = 'b a') {
+# Create function to extract data from a URL
+extract_data <- function(url, extract, sleep = FALSE) {
   result <- tryCatch({
     html_page <- read_html(url)
     
@@ -76,9 +16,49 @@ pull_names <- function(url, extract = 'b a') {
     return(NULL)  
   })
   
+  if (sleep){
+    Sys.sleep(runif(1, 1, 3)) # Randomized delay between 1 and 3 seconds to avoid overloading website
+  }
+  
   return(result)
 }
 
+#### SKYRIM ####
+url <- "https://en.uesp.net/wiki/Skyrim:People"
+
+# Note dragons and other creatures are not scraped in this search
+# This match will miss people from random encounters as these people are formatted in a list
+table_cells <- extract_data(url, 'td > b a')
+
+# Match for people who occur during random encounters
+random_encounter <- extract_data(url, 'li b a')
+
+# Remove duplicates (people might have lived in multiple different places resulting in multiple mentions)
+skyrim_people <- tibble(name = c(table_cells, random_encounter)) %>% 
+  group_by(name) %>% 
+  filter(row_number() == 1) %>% 
+  ungroup
+
+
+#### OBLIVION ####
+url <- "https://en.uesp.net/wiki/Oblivion:People"
+
+# This match will miss people who do not physically show up in the game
+# This will be fine as some of those names are joke names
+# Only miss out on an additional 40 potential names
+table_cells <- extract_data(url, '.wikitable dt a , b a')
+
+# Remove duplicates (people might have lived in multiple different places resulting in multiple mentions)
+oblivion_people <- tibble(name = c(table_cells)) %>% 
+  group_by(name) %>% 
+  filter(row_number() == 1) %>% 
+  ungroup
+
+
+#### MORROWIND ####
+
+# The morrowind layout is different. There are subpages the contain lists of npc names
+# Cycle through these pages since the layout in each subpage is the same
 list_of_urls <- c(
   'https://en.uesp.net/wiki/Morrowind:City_People',
   'https://en.uesp.net/wiki/Morrowind:Town_People',
@@ -98,9 +78,11 @@ list_of_urls <- c(
   'https://en.uesp.net/wiki/Morrowind:Wilderness_People'
 )
 
-tmp <- unlist(sapply(list_of_urls, pull_names), use.names = F)
-morrowind_people <- tibble(name = tmp)
-morrowind_people <- morrowind_people %>% 
+# This does miss some NPCs, but does get most of them
+# For example, there are a few here https://en.uesp.net/wiki/Morrowind:People_in_Balmora that are missed
+tmp <- unlist(sapply(list_of_urls, extract_data, extract = 'b a', sleep = TRUE), use.names = F)
+
+morrowind_people <- tibble(name = tmp) %>% 
   group_by(name) %>% 
   filter(row_number() == 1) %>% 
   ungroup
@@ -108,14 +90,10 @@ morrowind_people <- morrowind_people %>%
 
 #### DAGGERFALL ####
 url <- "https://en.uesp.net/wiki/Daggerfall:People"
-html_page <- read_html(url)
 
-table_cells <- html_page %>%
-  html_nodes(".wikitable a") %>%
-  html_text()
+table_cells <- extract_data(url, '.wikitable a')
 
-daggerfall_people <- tibble(name = table_cells)
-daggerfall_people <- daggerfall_people %>% 
+daggerfall_people <- tibble(name = table_cells) %>% 
   group_by(name) %>% 
   filter(row_number() == 1) %>% 
   ungroup
@@ -123,14 +101,10 @@ daggerfall_people <- daggerfall_people %>%
 #### ARENA ####
 
 url <- "https://en.uesp.net/wiki/Arena:People"
-html_page <- read_html(url)
 
-table_cells <- html_page %>%
-  html_nodes(".mw-headline") %>%
-  html_text()
+table_cells <- extract_data(url, '.mw-headline')
 
-arena_people <- tibble(name = table_cells)
-arena_people <- arena_people %>% 
+arena_people <- tibble(name = table_cells) %>% 
   group_by(name) %>% 
   filter(row_number() == 1) %>% 
   ungroup
@@ -139,24 +113,23 @@ arena_people <- arena_people %>%
 
 # This page has many subpages containing lists of people
 url <- "https://en.uesp.net/wiki/Online:People"
-html_page <- read_html(url)
 
 # Scrape for the names of those pages
-section_headers <- html_page %>%
-  html_nodes("h3+ ul a , h4+ ul a") %>%
-  html_text()
+section_headers <- extract_data(url, "h3+ ul a , h4+ ul a")
 
 # Create URLs from the page names
 section_urls <- paste0('https://en.uesp.net/wiki/Online:', str_replace_all(section_headers, ' ', '_'))
 
 # Extract names from each subpage
-tmp <- unlist(sapply(section_urls, pull_names, extract = '.wikitable b a'), use.names = F)
+tmp <- unlist(sapply(section_urls, extract_data, extract = '.wikitable b a', sleep = FALSE), use.names = F) # For some reason when using sleep some webpages fail, but is fine when sleep is not used. idk why this is happening
 
-eso_people <- tibble(name = tmp)
-eso_people <- eso_people %>% 
+eso_people <- tibble(name = tmp) %>% 
   group_by(name) %>% 
   filter(row_number() == 1) %>% 
   ungroup
+
+
+#### COMBINE ####
 
 # Combine all Elder Scrolls NPC names
 TES_names <- bind_rows(
@@ -168,7 +141,6 @@ TES_names <- bind_rows(
   eso_people,
   .id = 'game'
 ) %>% 
-  filter(name != "")  %>% 
   mutate(
     game = case_when(
       game == '1' ~ 'Skyrim',
@@ -180,44 +152,5 @@ TES_names <- bind_rows(
     )
   ) %>% 
   select(name, game)
-
-# Remove the prefix Jarl in all "Jarl [Name]" for Skyrim characters
-# The URL for non Skyrim (ESO) Jarls has their Jarl title in it, Skyrim does not
-TES_names <- TES_names %>% mutate(
-  name = case_when(
-    game == 'Skyrim' ~ str_replace_all(name, "^Jarl\\s+", ""),
-    TRUE ~ name
-  )
-) %>% group_by(name, game) %>% 
-  filter(row_number() == 1) %>% 
-  ungroup
-
-# Fun analysis of same names that were used in two different games
-# Keep all dups as the are different people
-TES_names %>% 
-  group_by(name) %>% 
-  filter(n() > 1) %>%
-  ungroup %>% 
-  arrange(name) 
-
-# Create a link to each NPC's online profile
-TES_names <- TES_names %>% 
-  mutate(
-    url = paste0(
-      'https://en.uesp.net/wiki/', 
-      game, ':',
-      str_replace_all(name, ' ', '_')
-    )
-  )
-
-# Now, after URL has been created, go back and remove Jarl title from all people
-TES_names <- TES_names %>% mutate(name = str_replace_all(name, "^Jarl\\s+", ""))
-
-# Will clean up this list more in depth, but for now, get rid of easily detectable unwanted names
-guards <- TES_names$name[str_detect(tolower(TES_names$name), tolower('Guards'))]
-initiate <- TES_names$name[str_detect(tolower(TES_names$name), tolower('.* Initiate'))]
-dlc <- TES_names %>% filter(name %in% c('CC', 'HF', 'DG', 'DB')) %>% pull(name)
-
-TES_names <- TES_names %>% filter(!(name %in% c(guards, initiate, dlc)))
 
 write_csv(TES_names, '~/elder-scrolls-name-generation/data/TES_names.csv')
