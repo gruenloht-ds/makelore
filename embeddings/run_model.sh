a15f685a9b51f9852922ce65e2e19b1735d6c3e5
gruenloht-ds <tim@gruenloh.org>
2025-01-02 23:58:36 -0600

    started training

--- embeddings/run_model.sh
+++ embeddings/run_model.sh
@@ -5,13 +5,14 @@ hidden_size=1024
 emb_size=5
 lr=1e-3
 batch_size=64
-epochs=10
+epochs=6000
 seed=42
 
 vocab_path="model_data/data/vocabulary.pkl"
 data_path="model_data/data/processed_data_w$window.pkl"
 save_model_path="model_data/NPLM-w$window-emb$emb_size-h$hidden_size-bs$batch_size-lr$lr"
 load_model_path="model_data/NPLM-w$window-emb$emb_size-h$hidden_size-bs$batch_size-lr$lr-10.pth"
+load_model_path=""
 
 eval_test="--eval-test"
 eval_test=""
